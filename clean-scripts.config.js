4e2d4de82003c530a1dd2e068e53a32253bd9a71
york yao <yaoao12306@outlook.com>
2017-09-06 14:13:39 +0800

    add sleep in test

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,4 +1,5 @@
 const childProcess = require('child_process')
+const { sleep } = require('clean-scripts')
 
 module.exports = {
   build: [
@@ -13,27 +14,24 @@ module.exports = {
   test: [
     'tsc -p spec',
     'jasmine',
-    () => new Promise((resolve, reject) => {
+    async () => {
       const fetch = require('node-fetch')
       const server = childProcess.spawn('node', ['./dist/start.js'])
       server.stdout.pipe(process.stdout)
       server.stderr.pipe(process.stderr)
-      setTimeout(() => {
-        fetch('http://localhost:9994/items')
-          .then(res => {
-            res.text().then(text => {
-              if (text !== '{"isSuccess":true,"items":[]}') {
-                throw new Error('Error when get items')
-              }
-              server.kill('SIGINT')
-              resolve()
-            })
-          }, error => {
-            server.kill('SIGINT')
-            reject(error)
-          })
-      }, 1000)
-    }),
+      await sleep(1000)
+      try {
+        const res = await fetch('http://localhost:9994/items')
+        const text = await res.text()
+        if (text !== '{"isSuccess":true,"items":[]}') {
+          throw new Error('Error when get items')
+        }
+        server.kill('SIGINT')
+      } catch (error) {
+        server.kill('SIGINT')
+        throw error
+      }
+    },
     () => new Promise((resolve, reject) => {
       childProcess.exec('git status -s', (error, stdout, stderr) => {
         if (error) {
