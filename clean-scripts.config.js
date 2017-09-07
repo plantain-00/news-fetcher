9773eaef16e06c16c59ff9fad25e39a8a6ff728d
york yao <yaoao12306@outlook.com>
2017-09-07 15:08:06 +0800

    more test

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -29,6 +29,25 @@ module.exports = {
         if (text !== '{"isSuccess":true,"items":[]}') {
           throw new Error('Error when get items')
         }
+
+        const saveResponse = await fetch('http://localhost:9994/items', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: '{"url": "http://localhost"}'
+        })
+        const saveText = await saveResponse.text()
+        if (saveText !== '{"isSuccess":true}') {
+          throw new Error('Error when save items')
+        }
+
+        const responseAfterSave = await fetch('http://localhost:9994/items')
+        const textAfterSave = await responseAfterSave.text()
+        if (textAfterSave !== '{"isSuccess":true,"items":["http://localhost"]}') {
+          throw new Error('Error when get items after save')
+        }
+
         server.kill('SIGINT')
       } catch (error) {
         server.kill('SIGINT')
