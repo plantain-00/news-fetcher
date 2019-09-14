092aeb3dc9f1fc65af602f4380ef18a4c92e779a
york yao <yaoao12306@outlook.com>
2019-09-14 19:00:28 +0800

    chore: change release

--- clean-release.config.js
+++ clean-release.config.js
@@ -11,8 +11,13 @@ module.exports = {
   askVersion: true,
   releaseRepository: 'https://github.com/plantain-00/news-fetcher-release.git',
   postScript: [
-    'cd "[dir]" && rm -rf .git',
-    'cp Dockerfile "[dir]"',
-    'cd "[dir]" && docker build -t plantain/news-fetcher . && docker push plantain/news-fetcher'
+    // 'cd "[dir]" && rm -rf .git',
+    // 'cp Dockerfile "[dir]"',
+    // 'cd "[dir]" && docker build -t plantain/news-fetcher . && docker push plantain/news-fetcher'
+    'git add package.json',
+    'git commit -m "[version]"',
+    'git tag v[version]',
+    'git push',
+    'git push origin v[version]',
   ]
 }
